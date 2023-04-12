b210e4f9b86178a737d0aef647ee900b0ed17469
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-04-12 20:55:05 +0200

    Listing Item completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -162,7 +162,7 @@ export default function Profile() {
             <h2 className="text-2xl text-center font-semibold mb-6">
               My Listings
             </h2>
-            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6 ">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
