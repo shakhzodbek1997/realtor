5e381492358b2bc2fa5028b5908905009e6cdf3b
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-04-14 16:50:31 +0200

    shared coppy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,12 +6,13 @@ import Spinner from '../components/Spinner';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
-
+import {FaShare} from 'react-icons/fa'
 
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false)
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
   useEffect(() => {
@@ -51,6 +52,19 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div 
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center "
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied &&  <p className="fixed top-[18%] right-[3%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link Copied</p>}
     </main>
   )
 }
