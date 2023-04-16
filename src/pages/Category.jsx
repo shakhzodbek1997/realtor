bd40bafde6e8e09dfb54dd5d948d53fa5a71eb9d
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-04-16 09:06:04 +0200

    fixed curren offers section

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -98,7 +98,11 @@ function Category() {
           )}
         </div>
       ) : (
-        <p>There are no current offers</p>
+        <p>There are no current {" "} 
+        {params.categoryName === "rent" ?
+          "places for rent" : 
+          "places for sale"
+        }</p>
       )}
     </div>
   )
